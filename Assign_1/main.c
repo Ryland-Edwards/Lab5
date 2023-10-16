c7f11992c692cff797128db459c9e30ebb9a203d
Ryland Edwards <rylandcedwards@gmail.com>
2023-10-16 13:33:11 -0500

    added comments

--- Assign_1/main.c
+++ Assign_1/main.c
@@ -14,14 +14,36 @@ struct SaleToMonth {
   char month[20];
 };
 
-double Avg(struct SaleToMonth sales[], int size){
-  double sum = 0;
-  for (int i = 0; i < size; i++){
+/**
+ * The function calculates the average sales from a given range of months.
+ * 
+ * @param sales The parameter "sales" is an array of structs named "SaleToMonth". Each struct in the
+ * array represents sales data for a specific month.
+ * @param size The size parameter represents the total number of elements in the sales array.
+ * @param start The start parameter represents the index of the first element in the sales array that
+ * should be included in the calculation of the average.
+ * @param stop The "stop" parameter represents the index at which the loop should stop iterating. It is
+ * the index of the last element in the range of the array that should be included in the calculation.
+ * 
+ * @return the average of the sales values in the given range.
+ */
+double Avg(struct SaleToMonth sales[], int size, int start, int stop){
+  double sum = 0.0;
+  for (int i = start; i < stop; i++)
+  {
     sum += sales[i].sales;
   }
-  return sum / size;
+  return (sum / (double)(stop-start));
 }
 
+/**
+ * The function "Min" finds the struct with the minimum sales value from an array of structs.
+ * 
+ * @param sales An array of structs of type SaleToMonth, representing sales data for each month.
+ * @param size The parameter "size" represents the number of elements in the array "sales".
+ * 
+ * @return the struct SaleToMonth object with the minimum sales value.
+ */
 struct SaleToMonth Min(struct SaleToMonth sales[], int size){
   struct SaleToMonth min = sales[0];
   for (int i = 1; i < size; i++){
@@ -32,7 +54,16 @@ struct SaleToMonth Min(struct SaleToMonth sales[], int size){
   return min;
 }
 
-struct SaleToMonth Maximum(struct SaleToMonth sales[], int size){
+/**
+ * The function "Max" finds the struct with the highest sales value in an array of structs.
+ * 
+ * @param sales An array of structs of type SaleToMonth, representing sales data for each month.
+ * @param size The parameter "size" represents the size of the array "sales". It indicates the number
+ * of elements in the array that should be considered for finding the maximum value.
+ * 
+ * @return the struct SaleToMonth object with the highest sales value.
+ */
+struct SaleToMonth Max(struct SaleToMonth sales[], int size){
   struct SaleToMonth max = sales[0];
   for (int i = 1; i < size; i++){
     if (sales[i].sales > max.sales){
@@ -42,6 +73,19 @@ struct SaleToMonth Maximum(struct SaleToMonth sales[], int size){
   return max;
 }
 
+/**
+ * The Compare function compares the sales values of two SaleToMonth structures.
+ * 
+ * @param a The parameter `a` is a pointer to a `void` type, which means it can point to any type of
+ * data. In this case, it is expected to point to a structure of type `SaleToMonth`.
+ * @param b The parameter `b` is a pointer to a constant void type. It is used to represent the second
+ * element being compared in the `Compare` function.
+ * 
+ * @return The Compare function returns an integer value. It returns -1 if the sales value of the
+ * second argument is less than the sales value of the first argument, 1 if the sales value of the
+ * first argument is less than the sales value of the second argument, and 0 if the sales values are
+ * equal.
+ */
 int Compare(const void *a, const void *b) {
   double val_1 = ((struct SaleToMonth *)a)->sales;
   double val_2 = ((struct SaleToMonth *)b)->sales;
@@ -51,14 +95,23 @@ int Compare(const void *a, const void *b) {
   return 0;
 }
 
+/**
+ * The function sorts an array of SaleToMonth structures using the qsort function.
+ * 
+ * @param sales sales is a pointer to an array of struct SaleToMonth objects.
+ * @param n The number of elements in the sales array.
+ */
 void sortSales(struct SaleToMonth *sales, size_t n) {
   qsort(sales, n, sizeof(struct SaleToMonth), Compare);
 }
 
 
-
-
+/**
+ * The above function is the main function in a C program.
+ */
 int main() {
+  /* This block of code is declaring and initializing variables that will be used to read data from a
+  file and store it in an array of structures. */
   FILE* input_file;
   struct SaleToMonth sales[12];
   char months[12][20] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
@@ -71,17 +124,31 @@ int main() {
   input_file = fopen("sales_input.txt", "r");
   while (!feof(input_file)) {
     file_ch = fgetc(input_file);
+    /* The code block `if (file_ch != EOF) { string[char_counter] = file_ch; char_counter++; }` is
+    checking if the character read from the file is not the end-of-file (EOF) character. If it is
+    not the end-of-file character, it means that there is still more data to be read from the file.
+    In that case, the character is stored in the `string` array at the current `char_counter` index,
+    and the `char_counter` is incremented to prepare for the next character to be read. This process
+    continues until the end-of-file character is encountered or the end of the line is reached. */
     if (file_ch != EOF) {
       string[char_counter] = file_ch;
       char_counter++;
     }
+    /* This code block is checking if the character read from the file is either a newline character
+    (`'\n'`) or the end-of-file character (`EOF`). If either of these conditions is true, it means
+    that a complete line of data has been read from the file. */
     if (file_ch == '\n' || file_ch == EOF){
       // Convert the string to a number and store it in the array of structures
       sales[month_counter].sales = strtod(string, &end_point);
 
+      /* The code `for (int i = 0; i < 20; i++)` is a loop that iterates from 0 to 19. It is used to
+      copy the characters from the `months` array to the `month` property of the `sales` struct. */
       for (int i = 0; i < 20; i++){
         sales[month_counter].month[i] = months[month_counter][i];
       }
+      /* The `strcpy(string, "");` statement is used to copy an empty string into the `string` array.
+      It essentially clears the contents of the `string` array, making it an empty string. This is
+      done to prepare the `string` array for storing a new line of data read from the file. */
       strcpy(string, "");
       month_counter++;
       char_counter = 0;
@@ -91,48 +158,60 @@ int main() {
   }
   fclose(input_file);
 
+  /* The code is declaring three variables: `Maximum`, `Minimum`, and `Average`. */
   struct SaleToMonth Maximum;
   struct SaleToMonth Minimum;
   double Average;
   
-  Maximum = Max(sales, 12);
-
+  /* The line `Minimum = Min(sales, 12);` is calling the `Min` function and assigning the returned
+  value to the variable `Minimum`. */
   Minimum = Min(sales, 12);
+
+  /* The line `Maximum = Max(sales, 12);` is calling the `Max` function and assigning the returned
+  value to the variable `Maximum`. The `Max` function finds the struct with the highest sales value
+  in an array of structs. In this case, it is finding the struct with the highest sales value from
+  the `sales` array, which contains sales data for each month. The `Max` function takes two
+  arguments: the `sales` array and the size of the array (which is 12 in this case). It returns the
+  struct `SaleToMonth` object with the highest sales value. */
+  Maximum = Max(sales, 12);
   
+  /* The line `Average = Avg(sales, 12, 0, 12);` is calculating the average sales value from the
+  `sales` array. */
   Average = Avg(sales, 12, 0, 12);
 
+  /* The code is printing a monthly sales report for the year 2022. */
   printf("\nMonthly Sales Report for 2022:\n\n");
   printf("%-8s \t\t %-8s\n\n", "Month", "Sales");
   for (int i = 0; i < 12; i++){
-    printf("%-8s \t\t $ %-8.2f\n", months[i], sales[i].sales);
+    printf("%-8s \t\t\t $ %-8.2f\n", months[i], sales[i].sales);
   }
 
-  
 
-  printf("\nMonthly Sales Report for 2022:\n");
-  printf("%-8s \t\t %-8s\n", "Month", "Sales");
-  for (int i = 0; i < 12; i++)
-  {
-    printf("%-8s \t\t $ %-8.2f\n", months[i], sales[i].sales);
-  }
 
-  // Summary
+  /* The `printf` statements are used to print the minimum sales, maximum sales, and average sales
+  values. */
   printf("\nMinimum sales: \t\t $ %-8.2f (%s)\n", Minimum.sales, Minimum.month);
   printf("Maxmimum sales: \t\t $ %-8.2f (%s)\n", Maximum.sales, Maximum.month);
   printf("Average sales: \t\t $ %-8.2f\n", Average);
 
-  // 6-Month Moving Average
+
+  /* The code block is generating a six-month moving average report. */
   char startMonth[20];
   char endMonth[20];
   printf("\nSix-Month Moving Average Report: \n");
   for (int i = 0; i < 7; i++)
   {
+    /* The `strcpy` function is used to copy a string from one location to another. In this case,
+    `strcpy(startMonth, months[i])` is copying the string stored in the `months[i]` array to the
+    `startMonth` array. Similarly, `strcpy(endMonth, months[i+5])` is copying the string stored in
+    the `months[i+5]` array to the `endMonth` array. */
     strcpy(startMonth, months[i]);
     strcpy(endMonth, months[i+5]);
-    printf("%-8s \t - \t %-8s \t\t $%-8.2d\n", startMonth, endMonth, Avg(sales, 12, i, i+6));
+    printf("%-8s \t - \t %-8s \t\t $%-8.2f\n", startMonth, endMonth, Avg(sales, 12, i, i+6));
   }
 
-  // Sorted Order
+
+  /* This code block is generating a sales report in descending order, from highest to lowest sales. */
   printf("\nSales Report (Highest to Lowest)\n");
   printf("%8s \t\t %8s", "Month", "Sales\n");
   sortSales(sales, 12);
--- Assign_2/main.c
+++ Assign_2/main.c
@@ -1,23 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define S 2
-#define FG 3
-#define TD 6
-#define TDFG 7
-#define TD2P 8
+/* The `#define` statements are used to define constants in the code. In this case, `S`, `FG`, `TD`,
+`TDFG`, and `TD2P` are defined as constants with the values 2, 3, 6, 7, and 8 respectively. These
+constants are used later in the code to represent the values of different scoring plays in a
+football game. */
+#define S 2 // Safetys
+#define FG 3 // Field goals
+#define TD 6 // Touchdown
+#define TDFG 7 // touchdown + field goal
+#define TD2P 8 // touchdown + conversion
 
 void nfl_score_combinations(int target_score) {
-    // Define the scoring plays and their values
+    
+    /* The `char *scoring_plays[]` is an array of strings that represents the different types of
+    scoring plays in a football game. Each element in the array corresponds to a specific scoring
+    play, such as a touchdown with a 2-point conversion, a touchdown with a field goal, a touchdown,
+    a 3-point field goal, or a safety. */
     char *scoring_plays[] = {"TD + 2pt", "TD + FG", "TD", "3pt FG", "Safety"};
     int values[] = {8, 7, 6, 3, 2};
 
-    // Iterate through all possible combinations
+    
+    /* The line `for (int td_2pt = 0; td_2pt <= target_score / values[0]; td_2pt++) {` is a for loop
+    that iterates over the possible number of touchdown + 2-point conversion combinations. */
     for (int td_2pt = 0; td_2pt <= target_score / values[0]; td_2pt++) {
+        /* The line `for (int td_fg = 0; td_fg <= target_score / values[1]; td_fg++) {` is a for loop
+        that iterates over the possible number of touchdown + field goal combinations. */
         for (int td_fg = 0; td_fg <= target_score / values[1]; td_fg++) {
+            /* The line `for (int td = 0; td <= target_score / values[2]; td++) {` is a for loop that
+            iterates over the possible number of touchdowns. */
             for (int td = 0; td <= target_score / values[2]; td++) {
+                /* The line `for (int fg = 0; fg <= target_score / values[3]; fg++) {` is a for loop
+                that iterates over the possible number of field goals in a scoring combination. */
                 for (int fg = 0; fg <= target_score / values[3]; fg++) {
+                    /* The line `for (int safety = 0; safety <= target_score / values[4]; safety++) {`
+                    is a for loop that iterates over the possible number of safeties in a scoring
+                    combination. */
                     for (int safety = 0; safety <= target_score / values[4]; safety++) {
+                        /* The `if` statement is checking if the current combination of scoring plays
+                        equals the target score. It does this by multiplying the number of each type
+                        of scoring play (touchdown + 2-point conversion, touchdown + field goal,
+                        touchdown, field goal, safety) by their respective values and summing them
+                        up. If the sum is equal to the target score, it means that the current
+                        combination of scoring plays adds up to the target score. */
                         if (td_2pt * values[0] + td_fg * values[1] + td * values[2] + fg * values[3] + safety * values[4] == target_score) {
                             printf("%d %s, %d %s, %d %s, %d %s, %d %s\n", td_2pt, scoring_plays[0], td_fg, scoring_plays[1], td, scoring_plays[2], fg, scoring_plays[3], safety, scoring_plays[4]);
                         }
@@ -28,25 +53,21 @@ void nfl_score_combinations(int target_score) {
     }
 }
 
-int main()
-{
-    
+int main(){
     int score = 2;
-    
-    
+    /* This code block is responsible for taking user input for an NFL score and then calling the
+    `nfl_score_combinations` function to calculate and display the possible combinations of scoring
+    plays that could result in that score. */
     printf("Enter 0 or 1 to STOP.\n");
     while (score > 1)
     {
         printf("Enter the NFL score: ");
-
-        // Get user input
+        /* `scanf("%d", &score);` is a function call that reads an integer input from the user and
+        stores it in the variable `score`. */
         scanf("%d", &score);
-
         printf("Possible combinations of scoring plays: \n");
         nfl_score_combinations(score);
         printf("\n");
-
     }
-
     return 0;
 }
